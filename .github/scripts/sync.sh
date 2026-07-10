578d0c12e5a4d823323ae9fe6fc8813627c746fb
y_potato <yangtudou1220@icloud.com>
2026-07-10 15:00:53 +0800

    Create sync.sh

---
+++ .github/scripts/sync.sh
@@ -0,0 +1,56 @@
+#!/usr/bin/env bash
+
+set -Eeuo pipefail
+
+readonly PLATFORM="${PLATFORM:-linux/amd64}"
+readonly REGISTRY="${ALIYUN_REGISTRY_DOMAIN}"
+readonly NAMESPACE="${ALIYUN_REGISTRY_SPACE_NAME}"
+
+success=0
+failed=0
+
+echo "========================================"
+echo " Docker Image Sync"
+echo "========================================"
+
+while IFS= read -r src || [[ -n "$src" ]]; do
+
+    # Ignore comments and empty lines
+    [[ -z "$src" || "$src" =~ ^# ]] && continue
+
+    image="${src##*/}"
+    name="${image%%:*}"
+    tag="${image#*:}"
+
+    [[ "$image" == "$tag" ]] && tag="latest"
+
+    dst="${REGISTRY}/${NAMESPACE}/${name}:${tag}"
+
+    echo
+    echo "━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━"
+    echo "📦 Source : $src"
+    echo "🎯 Target : $dst"
+
+    if crane copy \
+        --platform "$PLATFORM" \
+        "$src" \
+        "$dst"
+    then
+        ((success++))
+        echo "✅ Success"
+    else
+        ((failed++))
+        echo "❌ Failed"
+    fi
+
+done < .github/images.txt
+
+echo
+echo "========================================"
+echo " Summary"
+echo "----------------------------------------"
+echo " Success : $success"
+echo " Failed  : $failed"
+echo "========================================"
+
+(( failed == 0 ))
